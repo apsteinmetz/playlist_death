b468ef3114538e3c657c6508eaebbcc68effb45c
apsteinmetz <apsteinmetz@yahoo.com>
2020-02-01 17:13:50 -0500

    three features created

--- R/detect_deaths_1.R
+++ R/detect_deaths_1.R
@@ -1,4 +1,4 @@
-#library(xts)
+library(xts)
 library(tidyverse)
 library(tidymodels)
 library(lubridate)
@@ -6,7 +6,11 @@ library(lubridate)
 
 # pivot deaths so all bands are in one column though this will make names not unique.
 load("data/playlists.Rdata")
-playlists <- playlists %>% filter(ArtistToken != "")
+playlists <- playlists %>%
+  filter(ArtistToken != "") %>%
+  #  mutate(date_window = lubridate::interval(start = AirDate-14,end = AirDate))  %>%
+  mutate(date_window = as.yearqtr(AirDate))  %>%
+  {.}
 
 load("data/deaths.rdata")
 deaths <- deaths %>%
@@ -15,44 +19,50 @@ deaths <- deaths %>%
                values_drop_na = T) %>%
   select(-name) %>%
   mutate(band = str_trim(band)) %>%
+  mutate(date_window = as.yearqtr(Date)) %>%
   {.}
+
+#ha, ha
+# add name rows and band rows sequentially so we'll pick up both
+deaths_row <- deaths %>%
+  select(date_window,Name,band) %>%
+  pivot_longer(cols = c("Name","band"),names_to= "discard",values_to = "ArtistToken") %>%
+  select(-discard) %>%
+  {.}
+
 
 
 # artist spins by date
 # create interval of two weeks using lubridate
 
-# feature play count
-play_count<-playlists %>%
-  ungroup() %>%
-  filter(ArtistToken != "") %>%
-#  mutate(date_window = lubridate::interval(start = AirDate-14,end = AirDate))  %>%
-  mutate(date_window = quarter(AirDate,with_year = TRUE))  %>%
-  group_by(date_window,ArtistToken) %>%
-  summarise(spins = n()) %>%
-  {.}
 
 # feature dj count
 dj_counts<-playlists %>%
   ungroup() %>%
-  mutate(date_window = quarter(AirDate,with_year = TRUE))  %>%
-  group_by(ArtistToken,AirDate,DJ) %>%
+  group_by(ArtistToken,date_window,DJ) %>%
   summarise(Spins=n()) %>%
   summarise(dj_count=n()) %>%
   group_by(ArtistToken) %>%
   {.}
 
-#feature: artist concentration within shows
-temp <-playlists %>%
+#features: play absolute count and percentage
+all_plays <-playlists %>%
   ungroup() %>%
-  mutate(date_window = quarter(AirDate,with_year = TRUE))  %>%
-  group_by(DJ,AirDate) %>%
+  group_by(date_window) %>%
   summarise(total_spins = n()) %>%
-#  summarise(dj_count=n()) %>%
-#  group_by(ArtistToken) %>%
   {.}
-dj_concentration <-playlists %>%
+
+play_percentage <-playlists %>%
   ungroup() %>%
-  mutate(date_window = quarter(AirDate,with_year = TRUE))  %>%
-  group_by(DJ,AirDate,ArtistToken) %>%
+  group_by(date_window,ArtistToken) %>%
   summarise(spins = n()) %>%
+  left_join(all_plays,by = c("date_window")) %>%
+  group_by(date_window,ArtistToken) %>%
+  summarise(spins = sum(spins),total_spins = sum(total_spins)) %>%
+  mutate(concentration = spins/total_spins) %>%
+  select(-total_spins) %>%
   {.}
+
+artist_data <- left_join(play_percentage,dj_counts)
+
+artist_data_full <- full_join(artist_data,deaths_row)
