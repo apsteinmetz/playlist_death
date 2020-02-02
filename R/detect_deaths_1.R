2ea9796ce864e1ba69a04847863feb45fae7ab07
apsteinmetz <apsteinmetz@yahoo.com>
2020-02-02 13:17:10 -0500

    added list of problems with dataset. merged plays with deaths

--- R/detect_deaths_1.R
+++ R/detect_deaths_1.R
@@ -2,6 +2,19 @@ library(xts)
 library(tidyverse)
 library(tidymodels)
 library(lubridate)
+
+#problems to solve
+# 1. creating quarterly buckets is coarse and still risks an event right before the quarter boundary, splitting the
+# DJs who react across two quarters.  A rolling window scheme would avoid that but would increase the noise as
+# many windows might have signal
+
+# 2. need to create a "match any" scheme that still has one unique instance of any variants of artist name so we
+# don't create false missed events.
+# 3. Are artists that are in the death data but not in playlists not played at all or am I using a bad token
+# Say "E Street Band" counting as a miss even though "Bruce Springsteen" would be played.
+# In the former case I should set NAs to zero plays, in the latter I should delete rows with NAs.  I chose the latter.
+
+
 #feature engineering
 
 # pivot deaths so all bands are in one column though this will make names not unique.
@@ -27,6 +40,7 @@ deaths <- deaths %>%
 deaths_row <- deaths %>%
   select(date_window,Name,band) %>%
   pivot_longer(cols = c("Name","band"),names_to= "discard",values_to = "ArtistToken") %>%
+  mutate(event = factor("death",levels = c("death","not death"))) %>%
   select(-discard) %>%
   {.}
 
@@ -65,4 +79,16 @@ play_percentage <-playlists %>%
 
 artist_data <- left_join(play_percentage,dj_counts)
 
-artist_data_full <- full_join(artist_data,deaths_row)
+artist_data_full <- full_join(artist_data,deaths_row) %>%
+  mutate(event = fct_explicit_na(event,na_level = "not death")) %>%
+  drop_na() %>%
+  {.}
+
+
+
+# ---------- run models ----------------------------
+lm_spec <- logistic_reg(mode = "classification", penalty = NULL, mixture = NULL) %>%
+  set_engine("glm")
+
+
+
--- R/scratchpad.R
+++ R/scratchpad.R
@@ -10,3 +10,14 @@ df <- date %>% enframe(value = "date") %>%
 df %>% group_by(date) %>% count() %>% head(5)
 # NOT what I expect. group_by treats all intervals as one group
 df %>% group_by(interval) %>% count()
+
+At #rstudioconf @JennyBryan mentioned trying the same thing over and over as a bad debugging method. Just spent an hour trying:
+
+suppressPackageStartupMessages(library(dplyr))
+df <- dplyr::tibble(ndx = c(1,2,3),fct = as.factor(c("yay","ugh",NA)))
+
+#trying the same thing over
+df %>% dplyr::mutate(fct = ifelse(is.na(fct),"ugh",fct)) #this HAS to work WTF??
+
+#Other debugging technique, Google
+df %>% dplyr::mutate(fct = forcats::fct_explicit_na(fct,"ugh"))
